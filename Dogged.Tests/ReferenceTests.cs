4d7c84a11b4ace98567bd7d3b1aee1e85fed7b7d
Edward Thomson <ethomson@edwardthomson.com>
2019-03-20 09:03:16 -1000

    References: provide a reference enumerator

--- Dogged.Tests/ReferenceTests.cs
+++ Dogged.Tests/ReferenceTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Xunit;
 
@@ -48,6 +49,41 @@ namespace Dogged.Tests
                 Assert.IsType<DirectReference>(reference);
                 Assert.Equal("refs/heads/master", reference.Name);
                 Assert.Equal(new ObjectId("099fabac3a9ea935598528c27f866e34089c2eff"), ((DirectReference)reference).Target);
+            }
+        }
+
+        [Fact]
+        public void CanIterateReferences()
+        {
+            string[] expected = new string[] {
+                "refs/heads/br2",
+                "refs/heads/dir",
+                "refs/heads/executable",
+                "refs/heads/ident",
+                "refs/heads/long-file-name",
+                "refs/heads/master",
+                "refs/heads/merge-conflict",
+                "refs/heads/packed-test",
+                "refs/heads/subtrees",
+                "refs/heads/test",
+                "refs/heads/testrepo-worktree",
+                "refs/tags/e90810b",
+                "refs/tags/foo/bar",
+                "refs/tags/foo/foo/bar",
+                "refs/tags/point_to_blob",
+                "refs/tags/test",
+                "refs/heads/packed",
+                "refs/tags/packed-tag"
+            };
+            List<string> actual = new List<string>();
+
+            using (Repository repo = SandboxRepository("testrepo"))
+            {
+                foreach (Reference reference in repo.References)
+                using (reference)
+                {
+                    actual.Add(reference.Name);
+                }
             }
         }
     }
--- Dogged/ReferenceCollection.cs
+++ Dogged/ReferenceCollection.cs
@@ -8,7 +8,7 @@ namespace Dogged
     /// <summary>
     /// Accessors for the Git references (branches, tags) in a repository.
     /// </summary>
-    public class ReferenceCollection
+    public class ReferenceCollection : IEnumerable<Reference>
     {
         private Repository repository;
 
@@ -32,6 +32,107 @@ namespace Dogged
             Ensure.NativePointerNotNull(reference);
 
             return Reference.FromNative(reference);
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through references.
+        ///
+        /// <para>
+        /// Note that the returned references are <see cref="IDisposable"/>
+        /// and must be disposed properly.
+        /// </para>
+        /// </summary>
+        /// <returns>An enumerator of <see cref="IndexEntry"/> objects</returns>
+        public unsafe IEnumerator<Reference> GetEnumerator()
+        {
+            git_reference_iterator* iterator = null;
+            Ensure.NativeSuccess(() => libgit2.git_reference_iterator_new(out iterator, repository.NativeRepository), repository);
+            return ReferenceEnumerator.FromNative(iterator);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    /// <summary>
+    /// An enumerator for <see cref="Reference"/> objects.
+    /// </summary>
+    public class ReferenceEnumerator : NativeDisposable, IEnumerator<Reference>
+    {
+        private unsafe git_reference_iterator* nativeIterator;
+
+        private Reference current;
+
+        private unsafe ReferenceEnumerator(git_reference_iterator* nativeIterator)
+        {
+            Ensure.ArgumentNotNull(nativeIterator, "iterator");
+            this.nativeIterator = nativeIterator;
+        }
+
+        internal unsafe static ReferenceEnumerator FromNative(git_reference_iterator* nativeIterator)
+        {
+            return new ReferenceEnumerator(nativeIterator);
+        }
+
+        public void Reset()
+        {
+            /*
+             * IEnumerator.Reset is not required to be implemented and may throw
+             * a NotSupportedException.
+             *
+             * https://docs.microsoft.com/en-us/dotnet/api/system.collections.ienumerator.reset
+             */
+            throw new NotSupportedException("cannot reset a reference iterator");
+        }
+
+        public unsafe bool MoveNext()
+        {
+            git_reference *reference = null;
+            int ret = Ensure.NativeCall(() => libgit2.git_reference_next(out reference, nativeIterator), this);
+
+            if (ret == (int)git_error_code.GIT_ITEROVER)
+            {
+                return false;
+            }
+            Ensure.NativeSuccess(ret);
+
+            current = Reference.FromNative(reference);
+            return true;
+        }
+
+        public Reference Current
+        {
+            get
+            {
+                return current;
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        internal unsafe override bool IsDisposed
+        {
+            get
+            {
+                return (nativeIterator == null);
+            }
+        }
+
+        internal unsafe override void Dispose(bool disposing)
+        {
+            if (nativeIterator != null)
+            {
+                libgit2.git_reference_iterator_free(nativeIterator);
+                nativeIterator = null;
+            }
         }
     }
 }
